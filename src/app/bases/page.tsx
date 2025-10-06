74ada65c078ebbbd6c17e1136dbe605f48f1f1d2
LGXprod <mattghanno@gmail.com>
2025-10-06 22:22:23 +1100

    Added a users bases to the bases section of the ui

--- src/app/bases/page.tsx
+++ src/app/bases/page.tsx
@@ -15,35 +15,32 @@ export default async function Home() {
       .where(eq(bases.ownerId, session.user.id));
 
     return (
-      <div className="min-h-screen bg-neutral-300">
-        <div className="mx-auto max-w-7xl p-4">
-          <div className="mb-4 flex items-center justify-between">
-            <h1 className="text-2xl font-bold">Your Bases</h1>
-            <Link
-              href="/bases/new"
-              className="rounded bg-white px-4 py-2 font-semibold shadow hover:bg-neutral-100"
-            >
-              New Base
-            </Link>
-          </div>
-          {baseRows.length === 0 ? (
-            <p>You have no bases. Create one to get started!</p>
-          ) : (
-            <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-              {baseRows.map((base) => (
-                <li key={base.id} className="rounded bg-white p-4 shadow">
-                  <Link
-                    href={`/bases/${base.id}`}
-                    className="text-lg font-semibold hover:underline"
-                  >
-                    {base.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
+      <>
+        {baseRows.length === 0 ? (
+          <p>You have no bases. Create one to get started!</p>
+        ) : (
+          <ul className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {baseRows.map((base, i) => (
+              <Link
+                key={i}
+                href={`/bases/${base.id}`}
+                className="flex items-center gap-4 rounded-md border-1 border-neutral-200 bg-white px-6 py-4 shadow-2xs"
+              >
+                <div className="flex min-h-12 min-w-12 items-center justify-center rounded-md bg-rose-900">
+                  <p className="inline-block text-white">
+                    {base.name.slice(0, 2)}
+                  </p>
+                </div>
+
+                <div>
+                  <p className="mb-0.5 text-xs font-medium">{base.name}</p>
+                  <p className="text-xs">Opened just now</p>
+                </div>
+              </Link>
+            ))}
+          </ul>
+        )}
+      </>
     );
   }
 }
