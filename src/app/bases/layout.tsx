293af72921124e023c0de2b527df0c82395cd5e7
LGXprod <mattghanno@gmail.com>
2025-10-07 02:00:56 +1100

    Added menu account button and menu and airtable logo

--- src/app/bases/layout.tsx
+++ src/app/bases/layout.tsx
@@ -1,6 +1,8 @@
 import { auth } from "@/server/auth";
 import MenuBar from "../_components/MenuBar";
 import { redirect } from "next/navigation";
+import Image from "next/image";
+import UserAccount from "../_components/UserAccount";
 
 export default async function BasesLayout({
   children,
@@ -13,7 +15,14 @@ export default async function BasesLayout({
 
   return (
     <div className="flex h-screen flex-col bg-neutral-100">
-      <div className="min-h-14 w-full border-b-1 border-neutral-300 bg-white shadow-sm"></div>
+      <div className="flex min-h-14 w-full items-center justify-between border-b-1 border-neutral-300 bg-white px-4 py-2 shadow-sm">
+        <Image src="/images/logo.svg" alt="Logo" width={100} height={20} />
+
+        <UserAccount
+          name={session.user.name ?? "User"}
+          email={session.user.email ?? "No email"}
+        />
+      </div>
 
       <div className="flex h-full items-start justify-items-start">
         <MenuBar />
