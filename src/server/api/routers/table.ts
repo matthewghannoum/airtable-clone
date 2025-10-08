97da818e8ccda93230a9abd3e54e936e946e304a
LGXprod <mattghanno@gmail.com>
2025-10-08 19:12:03 +1100

    Added add row and get rows trpc functions

--- src/server/api/routers/table.ts
+++ src/server/api/routers/table.ts
@@ -22,4 +22,84 @@ export const tableRouter = createTRPCRouter({
         rows: rows.map((row) => row.values),
       };
     }),
+  addRow: protectedProcedure
+    .input(
+      z.object({
+        tableId: z.string(),
+        values: z.record(z.string(), z.union([z.string(), z.number()])),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const columns = await ctx.db
+        .select()
+        .from(airtableColumns)
+        .where(eq(airtableColumns.airtableId, input.tableId));
+
+      const columnIdToType: Record<string, "text" | "number"> = {};
+      for (const column of columns) {
+        columnIdToType[column.id] = column.type;
+      }
+
+      for (const [columnId, value] of Object.entries(input.values)) {
+        const expectedType = columnIdToType[columnId];
+
+        if (!expectedType) {
+          throw new Error(`Unknown column ID: ${columnId}`);
+        }
+
+        if (expectedType === "number" && !z.number().safeParse(value).success) {
+          throw new Error(`Expected number for column ${columnId}`);
+        }
+
+        if (expectedType === "text" && !z.string().safeParse(value).success) {
+          throw new Error(`Expected string for column ${columnId}`);
+        }
+      }
+
+      const [row] = await ctx.db
+        .insert(airtableRows)
+        .values({
+          airtableId: input.tableId,
+          values: input.values,
+        })
+        .returning();
+
+      if (!row) {
+        throw new Error("Failed to insert row");
+      }
+
+      return row;
+    }),
+  getRows: protectedProcedure
+    .input(z.object({ tableId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const rows = await ctx.db
+        .select()
+        .from(airtableRows)
+        .where(eq(airtableRows.airtableId, input.tableId));
+
+      const columns = await ctx.db
+        .select()
+        .from(airtableColumns)
+        .where(eq(airtableColumns.airtableId, input.tableId));
+
+      const columnIdToType: Record<string, "text" | "number"> = {};
+      for (const column of columns) {
+        columnIdToType[column.id] = column.type;
+      }
+
+      const cleanedRows = rows
+        .filter((row) => columnIdToType[row.id])
+        .map((row) => {
+          const values = row.values as Record<string, string | number | null>;
+
+          for (const { id: columnId } of columns) {
+            values[columnId] ??= null;
+          }
+
+          return { ...row, values };
+        });
+
+      return cleanedRows;
+    }),
 });
