779ed9e8a6d0a9bf7aad4721acf2155ddb840603
Matthew Ghannoum <mattghanno@gmail.com>
2025-10-09 17:08:58 +1100

    Feature/add rows (#5)
    * Added add row and get rows trpc functions
    
    * Updated add row function to be update row function
    
    * Added trpc function to an empty row
    
    * Updated new row creation to refetch the table data
    
    * Added optimistic update so empty row is shown immediately
    
    * Updated cells to be edittable ui only

--- src/app/_components/Airtable.tsx
+++ src/app/_components/Airtable.tsx
@@ -16,9 +16,43 @@ import {
   flexRender,
 } from "@tanstack/react-table";
 import { Tally5, TextInitial, Plus } from "lucide-react";
+import { useState } from "react";
 
 export default function Airtable({ tableId }: { tableId: string }) {
-  const { data: tableData } = api.table.get.useQuery({ tableId });
+  const utils = api.useUtils();
+
+  // For now the entire table will be refetched
+  // TODO: Create a row component that fetches and updates its own data
+  const { data: tableData, refetch } = api.table.get.useQuery({ tableId });
+
+  const createEmptyRow = api.table.createEmptyRow.useMutation({
+    onMutate: async () => {
+      // 1) stop outgoing refetches so we don't overwrite our optimistic change
+      await utils.table.get.cancel({ tableId });
+
+      // 2) snapshot previous cache
+      const prev = utils.table.get.getData({ tableId });
+
+      // 3) update cache optimistically
+      if (prev) {
+        const emptyRow = tableData?.columns.reduce(
+          (acc, col) => {
+            acc[col.id] = null;
+            return acc;
+          },
+          {} as Record<string, null>,
+        );
+
+        utils.table.get.setData({ tableId }, () => ({
+          columns: prev.columns,
+          rows: [...prev.rows, emptyRow],
+        }));
+      }
+
+      // 4) pass snapshot to error handler for rollback
+      return { prev };
+    },
+  });
 
   const rows = tableData ? tableData.rows : [];
 
@@ -32,6 +66,11 @@ export default function Airtable({ tableId }: { tableId: string }) {
       : [],
     getCoreRowModel: getCoreRowModel(),
   });
+
+  const [isCellEditing, setIsCellEditing] = useState<{
+    rowIndex: number;
+    columnId: string;
+  } | null>(null);
 
   return (
     <Table className="w-full border-collapse bg-white">
@@ -79,19 +118,50 @@ export default function Airtable({ tableId }: { tableId: string }) {
             {row.getVisibleCells().map((cell, index) => (
               <TableCell
                 key={cell.id}
-                className={
+                className={`${
                   index !== row.getVisibleCells().length - 1
                     ? "border-r border-neutral-300"
                     : ""
+                }`}
+                onClick={() =>
+                  setIsCellEditing({ rowIndex, columnId: cell.column.id })
                 }
               >
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                {isCellEditing?.rowIndex === rowIndex &&
+                isCellEditing?.columnId === cell.column.id ? (
+                  <input
+                    autoFocus
+                    className="bg-transparent outline-none"
+                    type={
+                      tableData?.columns.find(
+                        (col) => col.id === cell.column.id,
+                      )?.type === "number"
+                        ? "number"
+                        : "text"
+                    }
+                    defaultValue={cell.getValue() as string | number}
+                  />
+                ) : (
+                  flexRender(cell.column.columnDef.cell, cell.getContext())
+                )}
               </TableCell>
             ))}
           </TableRow>
         ))}
 
-        <TableRow className="cursor-pointer">
+        <TableRow
+          className="cursor-pointer"
+          onClick={() => {
+            createEmptyRow
+              .mutateAsync({ tableId })
+              .then(async () => {
+                await refetch();
+              })
+              .catch((err) => {
+                console.error(err);
+              });
+          }}
+        >
           <TableCell>
             <Plus size={15} />
           </TableCell>
--- src/server/api/routers/table.ts
+++ src/server/api/routers/table.ts
@@ -4,6 +4,33 @@ import { eq } from "drizzle-orm";
 import { z } from "zod";
 
 export const tableRouter = createTRPCRouter({
+  createEmptyRow: protectedProcedure
+    .input(
+      z.object({
+        tableId: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const columns = await ctx.db
+        .select()
+        .from(airtableColumns)
+        .where(eq(airtableColumns.airtableId, input.tableId));
+
+      const values: Record<string, null> = {};
+      for (const column of columns) {
+        values[column.id] = null;
+      }
+
+      const [newRow] = await ctx.db
+        .insert(airtableRows)
+        .values({
+          airtableId: input.tableId,
+          values,
+        })
+        .returning();
+
+      return newRow;
+    }),
   get: protectedProcedure
     .input(z.object({ tableId: z.string() }))
     .query(async ({ ctx, input }) => {
@@ -21,5 +48,89 @@ export const tableRouter = createTRPCRouter({
         columns,
         rows: rows.map((row) => row.values),
       };
+    }),
+  updateRow: protectedProcedure
+    .input(
+      z.object({
+        tableId: z.string(),
+        rowId: z.string(),
+        values: z.record(
+          z.string(),
+          z.union([z.string(), z.number(), z.null()]),
+        ),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const columns = await ctx.db
+        .select()
+        .from(airtableColumns)
+        .where(eq(airtableColumns.airtableId, input.tableId));
+
+      const columnIdToType: Record<string, "text" | "number"> = {};
+      for (const column of columns) {
+        columnIdToType[column.id] = column.type;
+      }
+
+      for (const [columnId, value] of Object.entries(input.values)) {
+        const expectedType = columnIdToType[columnId];
+
+        if (!expectedType) {
+          throw new Error(`Unknown column ID: ${columnId}`);
+        }
+
+        if (value === null) continue;
+
+        if (expectedType === "number" && !z.number().safeParse(value).success) {
+          throw new Error(`Expected number for column ${columnId}`);
+        }
+
+        if (expectedType === "text" && !z.string().safeParse(value).success) {
+          throw new Error(`Expected string for column ${columnId}`);
+        }
+      }
+
+      const [row] = await ctx.db
+        .update(airtableRows)
+        .set({ values: input.values })
+        .where(eq(airtableRows.id, input.rowId))
+        .returning();
+
+      if (!row) {
+        throw new Error("Failed to insert row");
+      }
+
+      return row;
+    }),
+  getRows: protectedProcedure
+    .input(z.object({ tableId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const rows = await ctx.db
+        .select()
+        .from(airtableRows)
+        .where(eq(airtableRows.airtableId, input.tableId));
+
+      const columns = await ctx.db
+        .select()
+        .from(airtableColumns)
+        .where(eq(airtableColumns.airtableId, input.tableId));
+
+      const columnIdToType: Record<string, "text" | "number"> = {};
+      for (const column of columns) {
+        columnIdToType[column.id] = column.type;
+      }
+
+      const cleanedRows = rows
+        .filter((row) => columnIdToType[row.id])
+        .map((row) => {
+          const values = row.values as Record<string, string | number | null>;
+
+          for (const { id: columnId } of columns) {
+            values[columnId] ??= null;
+          }
+
+          return { ...row, values };
+        });
+
+      return cleanedRows;
     }),
 });
