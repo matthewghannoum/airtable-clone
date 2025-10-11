2676c62dd01640de3525bf65e7d6386b3d0c7d16
LGXprod <mattghanno@gmail.com>
2025-10-11 19:16:45 +1100

    Added form to add a column

--- src/app/_components/Airtable/ATAddCol.tsx
+++ src/app/_components/Airtable/ATAddCol.tsx
@@ -1,41 +1,110 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { TableRow } from "@/components/ui/table";
 import { Plus, Tally5, TextInitial } from "lucide-react";
+import { useState } from "react";
 
 function ColOption({
   text,
   icon: Icon,
+  onClick,
 }: {
   text: string;
   icon: React.ReactNode;
+  onClick?: () => void;
 }) {
   return (
-    <div className="flex w-full items-center justify-start gap-2 p-1 hover:rounded-sm hover:bg-neutral-100">
+    <Button
+      onClick={onClick}
+      variant="ghost"
+      className="flex w-full cursor-pointer items-center justify-start gap-2"
+    >
       {Icon}
       <p className="text-sm">{text}</p>
-    </div>
+    </Button>
   );
 }
 
 export default function ATAddCol() {
+  const [isAddingCol, setIsAddingCol] = useState(false);
+  const [colType, setColType] = useState<"text" | "number" | null>(null);
+
   return (
     <TableRow>
-      <div className="flex h-10 min-w-24 items-center justify-center">
-        <Popover>
-          <PopoverTrigger>
+      <div className="flex h-10 min-w-24">
+        <Popover open={isAddingCol} onOpenChange={setIsAddingCol}>
+          <PopoverTrigger className="flex w-full cursor-pointer items-center justify-center">
             <Plus />
           </PopoverTrigger>
-          <PopoverContent sideOffset={10}>
-            <div className="flex flex-col gap-1">
-              <p className="mb-1 text-sm text-neutral-500">Standard fields</p>
 
-              <ColOption text="Text" icon={<TextInitial size={15} />} />
-              <ColOption text="Number" icon={<Tally5 size={15} />} />
-            </div>
+          <PopoverContent
+            sideOffset={10}
+            className="w-[min(var(--radix-popper-available-width),theme(maxWidth.sm))] p-4"
+          >
+            {!colType ? (
+              <div className="flex flex-col gap-1">
+                <p className="mb-1 text-sm text-neutral-500">Standard fields</p>
+
+                <ColOption
+                  onClick={() => setColType("text")}
+                  text="Text"
+                  icon={<TextInitial size={15} />}
+                />
+                <ColOption
+                  onClick={() => setColType("number")}
+                  text="Number"
+                  icon={<Tally5 size={15} />}
+                />
+              </div>
+            ) : (
+              <div className="flex flex-col gap-2">
+                <Input className="w-full" autoFocus placeholder="Field name" />
+
+                <Select
+                  value={colType}
+                  onValueChange={(value) =>
+                    setColType(value as "text" | "number")
+                  }
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Theme" />
+                  </SelectTrigger>
+
+                  <SelectContent>
+                    <SelectItem value="text">Text</SelectItem>
+                    <SelectItem value="number">Number</SelectItem>
+                  </SelectContent>
+                </Select>
+
+                <div className="flex w-full items-center justify-end gap-2">
+                  <Button
+                    variant="ghost"
+                    className="cursor-pointer"
+                    onClick={() => {
+                      setColType(null);
+                      setIsAddingCol(false);
+                    }}
+                  >
+                    Cancel
+                  </Button>
+                  <Button className="cursor-pointer bg-blue-600 hover:bg-blue-700">
+                    Create field
+                  </Button>
+                </div>
+              </div>
+            )}
           </PopoverContent>
         </Popover>
       </div>
