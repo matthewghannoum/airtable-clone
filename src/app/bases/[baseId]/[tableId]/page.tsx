b2a35a25bcd5942ea9b37cbcdbcc13a168efa817
LGXprod <mattghanno@gmail.com>
2025-10-26 01:59:56 +1100

    Updated container for table to be full height

--- src/app/bases/[baseId]/[tableId]/page.tsx
+++ src/app/bases/[baseId]/[tableId]/page.tsx
@@ -9,7 +9,7 @@ export default async function Page(
   // await new Promise((resolve) => setTimeout(resolve, 5000));
 
   return (
-    <div className="w-full">
+    <div className="h-full w-full">
       <Airtable tableId={tableId} />
     </div>
   );
