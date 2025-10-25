7d693411a693bb46ea7df53b10de7619b355baf2
LGXprod <mattghanno@gmail.com>
2025-10-25 16:52:36 +1100

    Replaced loading text with spinner component

--- src/app/bases/[baseId]/[tableId]/loading.tsx
+++ src/app/bases/[baseId]/[tableId]/loading.tsx
@@ -1,7 +1,9 @@
+import { Spinner } from "@/components/ui/spinner";
+
 export default function LoadingTable() {
   return (
     <div className="flex h-full w-full items-center justify-center text-sm text-neutral-500">
-      loading...
+      <Spinner className="size-5" />
     </div>
   );
 }
