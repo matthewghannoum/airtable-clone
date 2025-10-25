98976ac46a0a3744cd63591e759b3447625b547d
Matthew Ghannoum <mattghanno@gmail.com>
2025-10-25 15:48:38 +1100

    Optimize table layout loading experience

---
+++ src/app/bases/[baseId]/[tableId]/loading.tsx
@@ -0,0 +1,7 @@
+export default function LoadingTable() {
+  return (
+    <div className="flex h-full w-full items-center justify-center text-sm text-neutral-500">
+      loading...
+    </div>
+  );
+}
