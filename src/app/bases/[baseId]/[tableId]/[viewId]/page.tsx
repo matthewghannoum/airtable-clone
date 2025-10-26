edc22fc321f6fa2f4878d4e69f0e0bbe2bca6462
LGXprod <mattghanno@gmail.com>
2025-10-26 12:58:16 +1100

    Added viewId slug to the route

--- src/app/bases/[baseId]/[tableId]/[viewId]/page.tsx
+++ src/app/bases/[baseId]/[tableId]/[viewId]/page.tsx
@@ -1,16 +1,16 @@
 import Airtable from "@/app/_components/Airtable";
 
 export default async function Page(
-  props: PageProps<"/bases/[baseId]/[tableId]">,
+  props: PageProps<"/bases/[baseId]/[tableId]/[viewId]">,
 ) {
-  const { tableId } = await props.params;
+  const { tableId, viewId } = await props.params;
 
   // Uncomment below to add a 5 second delay to test loading ui
   // await new Promise((resolve) => setTimeout(resolve, 5000));
 
   return (
     <div className="h-full w-full">
-      <Airtable tableId={tableId} />
+      <Airtable tableId={tableId} viewId={viewId} />
     </div>
   );
 }
