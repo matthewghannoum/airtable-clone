8446dc7222b1d75743b85bc30d2af388673e7d9a
LGXprod <mattghanno@gmail.com>
2025-10-26 12:41:05 +1100

    Removed unused var

--- src/app/bases/[baseId]/[tableId]/page.tsx
+++ src/app/bases/[baseId]/[tableId]/page.tsx
@@ -3,7 +3,7 @@ import Airtable from "@/app/_components/Airtable";
 export default async function Page(
   props: PageProps<"/bases/[baseId]/[tableId]">,
 ) {
-  const { baseId, tableId } = await props.params;
+  const { tableId } = await props.params;
 
   // Uncomment below to add a 5 second delay to test loading ui
   // await new Promise((resolve) => setTimeout(resolve, 5000));
