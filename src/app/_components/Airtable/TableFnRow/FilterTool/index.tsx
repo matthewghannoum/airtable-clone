9121d5c86c1c8d701dad19c9a61da0b6f264a7d6
LGXprod <mattghanno@gmail.com>
2025-11-08 20:29:27 +1100

    Removed test data

--- src/app/_components/Airtable/TableFnRow/FilterTool/index.tsx
+++ src/app/_components/Airtable/TableFnRow/FilterTool/index.tsx
@@ -19,9 +19,6 @@ export default function FilterTool({
   viewId: string;
   columns: Column[];
 }) {
-  const conditionGroupMap = useConditions((state) => state.conditionTree);
-  const filters = useConditions((state) => state.filters);
-
   const setColumns = useConditions((state) => state.setColumns);
   const addCondition = useConditions((state) => state.addCondition);
   const createNewConditionGroup = useConditions(
@@ -29,35 +26,6 @@ export default function FilterTool({
   );
 
   useEffect(() => setColumns(columns), [columns]);
-
-  useEffect(() => {
-    addCondition("root", {
-      columnId: "27a1f78c-5347-493e-83a2-90faae43e70e", // name
-      columnType: "text",
-      operator: "equal-to",
-      value: "work",
-    });
-
-    const groupId = createNewConditionGroup("root", "or");
-
-    addCondition(groupId, {
-      columnId: "a543e30d-e5c2-4149-a191-e368896f1aa0", // num prs
-      columnType: "number",
-      operator: "gt",
-      value: 2,
-    });
-    addCondition(groupId, {
-      columnId: "81c0bf0c-1bfa-43c7-b5ed-67f20af3ed1b", // notes
-      columnType: "text",
-      operator: "equal-to",
-      value: "case 2",
-    });
-  }, []);
-
-  useEffect(
-    () => console.log("condition map", conditionGroupMap, filters),
-    [conditionGroupMap, filters],
-  );
 
   return (
     <Popover>
