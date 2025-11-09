e2f3fadbff1756a6af3126666061dc2f73f1c7ba
Matthew Ghannoum <mattghanno@gmail.com>
2025-11-09 16:02:36 +1100

    Create Airtable Clone themed login screen (#29)
    * Create Airtable Clone themed login screen
    
    * Redirect unauthenticated home to sign-in page
    
    * Use NextAuth signIn action for Google auth
    
    * Use NextAuth signOut in user menu
    
    * Await signin search params
    
    * Updated cursor to be pointer for google sign in and sign out buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,10 @@
-import Link from "next/link";
-
 import { auth } from "@/server/auth";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
 
-  if (session) redirect("/bases");
+  if (!session) redirect("/api/auth/signin");
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <Link
-          href={session ? "/api/auth/signout" : "/api/auth/signin"}
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-        >
-          {session ? "Sign out" : "Sign in"}
-        </Link>
-      </div>
-    </main>
-  );
+  redirect("/bases");
 }
